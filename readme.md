4d2b7b157a44f82c1d8bda13629de5fa59dcf0b5
gongysh <gongysh@node.testgong1.cord-testdrive-pg0.wisc.cloudlab.us>
2022-11-27 11:09:18 -0600

    add some information to run the demo

--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
 # umi-blog-example
+add:
+JWT_SECRET='dfadfadsf'
+UPSTASH_REDIS_REST_URL=''
+DATABASE_URL=
 
+to .env
 An example of blog website built with Umi.js and new API route feature in Umi 4. (WIP) 
+
+
+pnpm i umi-plugin-tailwindcss
+chmod +x node_modules/tailwindcss/lib/cli.js
--- src/api/login.ts
+++ src/api/login.ts
@@ -4,6 +4,7 @@ import bcrypt from "bcryptjs";
 import { signToken } from "@/utils/jwt";
 
 export default async function (req: UmiApiRequest, res: UmiApiResponse) {
+  console.log("xxxxxxxx", req.body)
   switch (req.method) {
     case 'POST':
       try {
@@ -11,16 +12,21 @@ export default async function (req: UmiApiRequest, res: UmiApiResponse) {
         const user = await prisma.user.findUnique({
           where: { email: req.body.email }
         });
+	console.log('dfadfsdaf', user);
         if (!user || !bcrypt.compareSync(req.body.password, user.passwordHash)) {
           return res.status(401).json({
             message: 'Invalid email or password'
           });
         }
+        console.log('2222222222', user);
         res.status(200)
-          .setCookie('token', await signToken(user.id))
+	  .setCookie('token', await signToken(user.id))
           .json({ ...user, passwordHash: undefined });
+        console.log('33333333-1');
         await prisma.$disconnect()
+        console.log('33333333-2');
       } catch (error: any) {
+        console.log('33333333', error);
         res.status(500).json(error);
       }
       break;
